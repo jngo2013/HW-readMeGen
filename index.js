59db2c5e992c6db552c14d88f057724036c778e7
Jimmy Ngo <jngo2013@gmail.com>
2020-04-09 22:05:11 -0700

    answers to questions now append to README file

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
+// require inquire and fs
 const inquirer = require("inquirer");
-const fs = require ("fs")
+const fs = require("fs");
+//inquirer questions
+inquirer.prompt([
+    {
+        type: "input",
+        name: "email",
+        message: "what is your github email?"
+    },
+    {
+        type: "input",
+        name: "picture",
+        message: "Link to github profile picture"
+    }
+]).then(answers => {
+    fs.appendFile("README.md",JSON.stringify(answers),err =>{
+    if(err){
+        console.log(err);
+        throw err 
 
-inquirer.prompt
+    }
+    console.log("success");
+    });
+});
 
 
 
 
-const questions = [
 
-];
+// const questions = [
 
-function writeToFile(fileName, data) {
-}
+// ];
 
-function init() {
+// function writeToFile(fileName, data) {
+// }
 
-}
+// function init() {
 
-init();
+// }
+
+// init();
