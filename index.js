cd7ded30a325cd3b46426864357fa7c0e28daff7
Jimmy Ngo <jngo2013@gmail.com>
2020-04-09 20:13:45 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const inquirer = require("inquirer");
+const fs = require ("fs")
+
+inquirer.prompt
+
+
+
+
+const questions = [
+
+];
+
+function writeToFile(fileName, data) {
+}
+
+function init() {
+
+}
+
+init();
